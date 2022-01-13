92b6ff32aa71bccf40d97fc3ffd7d2e3bd726b4f
davi33-emanoel <emanoeldavi348@gmail.com>
2022-01-13 19:23:15 -0300

    url declarada private

--- src/main/java/com/sat/apisat/ClienteRest.java
+++ src/main/java/com/sat/apisat/ClienteRest.java
@@ -5,7 +5,6 @@ import com.sat.apisat.database.RespositorioClientes;
 import com.sat.apisat.entidade.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,47 +14,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @RestController
 @RequestMapping("/clients")
 public class ClienteRest {
-	@SuppressWarnings("deprecation")
-	@Bean
+    private final String frontAngular = "https://angular-sat.herokuapp.com/";
+    @SuppressWarnings("deprecation")
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://angular-sat.herokuapp.com/").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                registry.addMapping("/**").allowedOrigins(frontAngular).allowedMethods("GET", "POST", "PUT", "DELETE",
+                        "OPTIONS", "HEAD", "TRACE", "CONNECT");
             }
         };
     }
+
     @Autowired
     private RespositorioClientes repositorio;
 
     @GetMapping
-    public List<Clientes>listar(){
+    public List<Clientes> listar() {
         return repositorio.findAll();
     }
+
     @GetMapping("/client/{id}")
-    public Clientes listaClientePorId(@PathVariable(value="id")long id) {
-    	return repositorio.findById(id);
+    public Clientes listaClientePorId(@PathVariable(value = "id") long id) {
+        return repositorio.findById(id);
     }
 
     @PostMapping("/save")
-    public Clientes salvar(@RequestBody Clientes clientes){
+    public Clientes salvar(@RequestBody Clientes clientes) {
         return repositorio.save(clientes);
     }
 
     @PutMapping("/change")
-    public Clientes alteraCLiente(@RequestBody Clientes clientes){
+    public Clientes alteraCLiente(@RequestBody Clientes clientes) {
         return repositorio.save(clientes);
     }
-    
-    @DeleteMapping("/delete/{id}")  
-    public Clientes deletaCliente(@PathVariable(value="id") long id){
-       return repositorio.deleteById(id);
+
+    @DeleteMapping("/delete/{id}")
+    public Clientes deletaCliente(@PathVariable(value = "id") long id) {
+        return repositorio.deleteById(id);
     }
 }
